3e6607b756de188d7e8a71408a7a36cafe3b7379
Karim Shoff <k4r1mch1k@gmail.com>
2020-06-23 21:19:41 +0500

    start project, customization of collectors, eslint, etc

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require("gulp");
+const pug = require("gulp-pug");
+const concat = require("gulp-concat");
+// const rename = require("gulp-rename");
+// const uglify = require("gulp-uglify");
+const autoprefixer = require("gulp-autoprefixer");
+const sass = require("gulp-sass");
+
+let src = {
+  dev: {
+    pug: "app/dev/pug",
+    sass: "app/dev/sass",
+    js: "app/dev/js",
+  },
+  prod: {
+    html: "app/prod/",
+    css: "app/prod/css",
+    js: "app/prod/js",
+  },
+};
+
+gulp.task("pug", function () {
+  return gulp
+    .src(src.dev.pug)
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(gulp.dest(src.prod.html));
+});
+
+gulp.task("sass", function () {
+  return gulp
+    .src(src.dev.sass)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(src.prod.css));
+});
+
+gulp.task("scripts", function () {
+  return gulp
+    .src(src.dev.js)
+    .pipe(concat("index.js"))
+    .pipe(gulp.dest(src.prod.css));
+});
+
+gulp.task("autopre", function () {
+  return gulp
+    .src(src.dev.sass)
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest(src.prod.css));
+});
