a4ea0c420f8e1e67797e8175abff67cf47d368de
Karim Shoff <k4r1mch1k@gmail.com>
2020-08-03 20:58:54 +0500

    idk

--- app/prod/js/script.js
+++ app/prod/js/script.js
@@ -32,8 +32,10 @@ let validObj = {
     },
   },
 };
-let btn = document.querySelector(".btn");
-btn.addEventListener("click", validator);
+
+
+let inputsList = document.querySelectorAll(".block__input");
+
 
 function RegCheck(reg, val, input) {
   if (!reg.test(val)) {
@@ -50,52 +52,70 @@ function CreateErrorMessage(input) {
   mesError.innerHTML = `Incorrect ${input.id}`;
   input.before(mesError);
   input.classList.add("error");
-  btn.addEventListener("click", validator);
 }
-function validator(evt) {
-  evt.preventDefault();
-  console.log(1);
-  btn.removeEventListener("click", validator);
-  let inputs = document.querySelectorAll(".block__input");
-  inputs.forEach(function (input) {
-    let val = input.value;
-    switch (input.getAttribute("id")) {
-      case "name":
-        let nameReg = new RegExp(validObj.nickname.reg);
-        if (val.length <= 4 || val.length >= 16) {
-          CreateErrorMessage(input);
-          RegCheck(nameReg, val, input);
-        }
-        break;
-      case "email":
-        let emailReg = new RegExp(validObj.email.reg);
-        RegCheck(emailReg, val, input);
-        break;
-      case "password":
-        let passwordReg = new RegExp(validObj.password.reg.b6);
-        RegCheck(passwordReg, val, input);
-        break;
-      case "date":
-        let dateReg = new RegExp(validObj.date.reg.first);
-        RegCheck(dateReg, val, input);
-        break;
-      case "tel":
-        let telReg = new RegExp(validObj.tel.reg.russian);
-        RegCheck(telReg, val, input);
-        break;
-      case "check":
-        if (!input.checked) CreateErrorMessage(input);
-        break;
-      case "file":
-        let file = input.files[0];
-        if (val === "") CreateErrorMessage(input);
-        else if (!file.type.startsWith(validObj.file.type.image)) {
-          CreateErrorMessage(input);
-          if (!(file.size < validObj.file.size.mb10)) CreateErrorMessage(input);
-        }
-        break;
-      default:
-        console.log("def");
-    }
+var validatorInit = function(){
+  let inputsList = document.querySelectorAll(".block__input");
+  inputsList.forEach(function(input){
+    input.addEventListener('blur', function(){
+      let val = input.value;
+      switch (input.getAttribute("id")) {
+        case "name":
+          let nameReg = new RegExp(validObj.nickname.reg);
+          if (val.length <= 4 || val.length >= 16) {
+            CreateErrorMessage(input);
+            RegCheck(nameReg, val, input);
+          }
+          break;
+        case "email":
+          let emailReg = new RegExp(validObj.email.reg);
+          RegCheck(emailReg, val, input);
+          break;
+        case "password":
+          let passwordReg = new RegExp(validObj.password.reg.b6);
+          RegCheck(passwordReg, val, input);
+          break;
+        case "date":
+          let dateReg = new RegExp(validObj.date.reg.first);
+          RegCheck(dateReg, val, input);
+          break;
+        case "tel":
+          let telReg = new RegExp(validObj.tel.reg.russian);
+          RegCheck(telReg, val, input);
+          break;
+        case "check":
+          if (!input.checked) CreateErrorMessage(input);
+          break;
+        case "file":
+          let file = input.files[0];
+          if (val === "") CreateErrorMessage(input);
+          else if (!file.type.startsWith(validObj.file.type.image)) {
+            CreateErrorMessage(input);
+            if (!(file.size < validObj.file.size.mb10)) CreateErrorMessage(input);
+          }
+          break;
+        default:
+          console.log("def");
+      }
+    })
   });
-}
+}();
+
+
+
+// function validator() {
+//   for (
+//     var _iterator = inputsList[Symbol.iterator](), _step; 
+//     !(_iteratorNormalCompletion = (_step = _iterator.next()).done);  
+//     _iteratorNormalCompletion = true){
+//       var input = _step.value;
+//       input.addEventListener('blur', function(){
+//         console.log(this);
+//       })
+//   }
+  
+  // console.log(this);
+  // let inputs = document.querySelectorAll(".block__input");
+  // inputs.forEach(function (input) {
+
+  // });
+// }
