5c82ea9973ccba30e91357e5f29f7c5dafd15f98
Karim Shoff <k4r1mch1k@gmail.com>
2020-08-08 17:09:54 +0500

    add render error message

--- app/prod/js/validator.js
+++ app/prod/js/validator.js
@@ -2,6 +2,7 @@ const getTemplate = (inputs = [], btn = []) => {
 
   const id = inputs.map(i => {
     return `<div class="block">
+              <div class="block__error error-${i.id}">${i.error}</div>
               <input class="${i.class} ${i.id}" type="${i.type}" placeholder="${i.placeholder}" id="${i.id}"/>
               <label class="block__label label-${i.id}" for="${i.id}">${i.label}</label>
             </div>`
@@ -31,13 +32,13 @@ class Validator {
 }
 let valid = new Validator (".validator-form", {
   inputs: [
-    {id: "email", type: "text", class: "block__input", placeholder: "enter your email", label: "enter your email"},
-    {id: "name", type: "text", class: "block__input", placeholder: "enter your name", label: "enter your name"},
-    {id: "password", type: "password", class: "block__input", placeholder: "password", label: "password"},
-    {id: "number", type: "tel", class: "block__input", placeholder: "number", label: "number"},
-    {id: "date", type: "date", class: "block__input", placeholder: "date", label: "date"},
-    {id: "checkbox", type: "checkbox", class: "block__input", placeholder: "checkbox", label: "checkbox"},
-    {id: "file", type: "file", class: "block__input", placeholder: "file", label: "file"}
+    {id: "email", type: "text", class: "block__input", placeholder: "enter your email", label: "enter your email", error: "incorrect name"},
+    {id: "name", type: "text", class: "block__input", placeholder: "enter your name", label: "enter your name", error: "incorrect email"},
+    {id: "password", type: "password", class: "block__input", placeholder: "password", label: "password", error: "incorrect password"},
+    {id: "number", type: "tel", class: "block__input", placeholder: "number", label: "number", error: "incorrect number"},
+    {id: "date", type: "date", class: "block__input", placeholder: "date", label: "date", error: "incorrect date"},
+    {id: "checkbox", type: "checkbox", class: "block__input", placeholder: "checkbox", label: "checkbox", error: "incorrect check"},
+    {id: "file", type: "file", class: "block__input", placeholder: "file", label: "file", error: "incorrect file"}
 
   ],
   btn: [
