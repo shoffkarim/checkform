6df1a5441f55d8bc943bd4f5c71a9b7c2be42d68
Karim Shoff <k4r1mch1k@gmail.com>
2020-08-09 11:29:52 +0500

    animation label for textarea

--- app/prod/js/validator.js
+++ app/prod/js/validator.js
@@ -1,6 +1,6 @@
 const getTemplate = (inputs = [], textarea = [], btn = []) => { // function what return inputs, button, and full form
 
-  const id = inputs.map(i => {
+  const input = inputs.map(i => { // template of input
     return `<div class="block">
               <div class="block__error error-${i.id}">${i.error}</div>
               <input class="validator ${i.class} ${i.id}" data-type="${i.id}" type="${i.type}" placeholder="${i.placeholder}" id="${i.id}"/>
@@ -8,19 +8,19 @@ const getTemplate = (inputs = [], textarea = [], btn = []) => { // function what
             </div>`
   });
 
-  const text = textarea.map(i => {
+  const text = textarea.map(i => { //template of textarea
     return `<div class="block">
               <div class="block__error error-${i.id}">${i.error}</div>
-              <textarea class="block__input ${i.class}" placeholder="${i.placeholder}" id="${i.id}"></textarea>
+              <textarea class="validator block__input ${i.class}" placeholder="${i.placeholder}" id="${i.id}"></textarea>
               <label class="block__label label-${i.id}" for="${i.id}">${i.label}</label>
             </div>`
   });
 
-  const button = btn.map(i => {
+  const button = btn.map(i => { // template button
     return `<button class="${i.class}" type="${i.type}">${i.text}</button>`
   });
   return `<form>
-            ${id.join('')}
+            ${input.join('')}
             ${text.join('')}
             ${button.join('')}
           </form>`;
@@ -52,12 +52,18 @@ class Validator {
     const input = event.target;
     switch (event.type) {
       case "focus":
+        if (input.id == "date") input.type = "date"
         input.classList.add("js-input-focus");
         break;
       case "blur":
         if(input.value != "") {
           input.classList.add("js-input-focus");
-        } else input.classList.remove("js-input-focus");
+        } else {
+          if (input.id == "date") {
+            input.type = "text";
+          }
+          input.classList.remove("js-input-focus");
+        }
         break;
       default:
         break;
@@ -70,7 +76,7 @@ let valid = new Validator (".validator-form", { //init class
     {id: "name", type: "text", class: "block__input", placeholder: "enter your name", label: "enter your name", error: "incorrect email"},
     {id: "password", type: "password", class: "block__input", placeholder: "password", label: "password", error: "incorrect password"},
     {id: "number", type: "tel", class: "block__input", placeholder: "number", label: "number", error: "incorrect number"},
-    {id: "date", type: "date", class: "block__input", placeholder: "date", label: "date", error: "incorrect date"},
+    {id: "date", type: "text", class: "block__input", placeholder: "date", label: "date", error: "incorrect date"}, // must indicate type text for date, for good animation
     {id: "checkbox", type: "checkbox", class: "block__input", placeholder: "checkbox", label: "checkbox", error: "incorrect check"},
     {id: "file", type: "file", class: "block__input", placeholder: "file", label: "file", error: "incorrect file"}
 
