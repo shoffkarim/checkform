3916a12d8491b3ff175fff3a8ecafebb70d92a8b
Karim Shoff <k4r1mch1k@gmail.com>
2020-09-06 17:45:50 +0500

    change configs

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,12 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
-const concat = require("gulp-concat");
-// const rename = require("gulp-rename");
-// const uglify = require("gulp-uglify");
-const autoprefixer = require("gulp-autoprefixer");
-const sass = require("gulp-sass");
 
 let src = {
   dev: {
     pug: "app/dev/pug/*.pug",
-    sass: "app/dev/sass/*.sass",
-    js: "app/dev/js/*.js",
   },
   prod: {
     html: "app/prod/",
-    css: "app/prod/css/",
-    js: "app/prod/js/",
   },
 };
 
@@ -28,29 +19,4 @@ gulp.task("pug", function () {
       })
     )
     .pipe(gulp.dest(src.prod.html));
-});
-
-gulp.task("sass", function () {
-  return gulp
-    .src(src.dev.sass)
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest(src.prod.css));
-});
-
-gulp.task("scripts", function () {
-  return gulp
-    .src(src.dev.js)
-    .pipe(concat("index.js"))
-    .pipe(gulp.dest(src.prod.css));
-});
-
-gulp.task("autopre", function () {
-  return gulp
-    .src(src.prod.css)
-    .pipe(
-      autoprefixer({
-        cascade: false,
-      })
-    )
-    .pipe(gulp.dest(src.prod.css));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,71 @@
 const path = require('path');
+const webpack = require('webpack');
+const uglifyJsPlugin = require("babel-minify-webpack-plugin");
+const ExtractTextPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, 'app/dev'),
-  mode: 'development',
-  entry: {
-    formcheck: ['@babel/polyfill', './js/script.js']
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'app/prod/js')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              '@babel/preset-env'
-            ],
-            plugins: [
-              "@babel/plugin-proposal-class-properties"
-            ]
-          }
-        }
-      }
-    ]
-  }
+    entry: './app/prod/js/checkform.js',
+    performance: {
+        hints: false
+    },
+    module: {
+        rules: [{
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            },
+            {
+                test: /\.(sass)$/,
+                use: [
+                    ExtractTextPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(css)$/,
+                use: [
+                    ExtractTextPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                ]
+            }
+            // {
+            //     test: /\.(svg|eot|woff|woff2|ttf)$/,
+            //     loader: 'file-loader'
+            // }
+        ]
+    },
+    plugins: [
+        new uglifyJsPlugin(),
+        new ExtractTextPlugin({
+            filename: 'css/main.css'
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: __dirname + '/app/prod',
+        publicPath: '/',
+        filename: 'js/script.js'
+    },
+    devServer: {
+        contentBase: './'
+    }
 };
