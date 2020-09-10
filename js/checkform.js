3cf1714147a252a68a3274bd7802084103bd2587
shoffkarim <k4r1mch1k@gmail.com>
2020-09-10 09:48:54 +0500

    refactoring code

--- js/checkform.js
+++ js/checkform.js
@@ -1,126 +1,150 @@
+let validObj = { // object parameters of validation
+  nickname: {
+    reg: /^[a-zA-Z]/,
+    minLength: 1,
+    maxLength: 15
+  },
+  fullName: {
+    reg: /^[а-яА-ЯёЁ]/,
+    minLength: 1,
+    maxLength: 15
+  },
+  email: {
+    reg: /^(?!.*@.*@.*$)(?!.*@.*--.*..*$)(?!.*@.*-..*$)(?!.*@.*-$)(.*@.+(..{1,11})?)$/,
+    minLength: 1,
+  },
+  password: {
+    reg: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{6,}/, //min 6 chars
+  },
+  date: {
+    reg: /^\d{4}[./-]\d{2}[./-]\d{2}$/, // first type gggg-mm-dd and sec type dd-mm-gggg /^\d{2}[./-]\d{2}[./-]\d{4}$/
+  },
+  tel: {
+    // eslint-disable-next-line no-useless-escape
+    reg: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/, // russian tel and american tel /^\+?(\d{1,3})?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/,
+    minLength: 1,
+    maxLength: 20
+  },
+  file: {
+    size: 10025711, // 10mb
+    type: "image",
+  },
+};
+// eslint-disable-next-line no-unused-vars
 class CheckForm {
-  validObj = { // object parameters of validation
-    nickname: {
-      reg: /^[a-zA-Z]/,
-      minLength: 1,
-      maxLength: 15
-    },
-    fullName: {
-      reg: /^[а-яА-ЯёЁ]/,
-      minLength: 1,
-      maxLength: 15
-    },
-    email: {
-      reg: /^(?!.*@.*@.*$)(?!.*@.*--.*..*$)(?!.*@.*-..*$)(?!.*@.*-$)(.*@.+(..{1,11})?)$/,
-      minLength: 1,
-    },
-    password: {
-      reg: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{6,}/, //min 6 chars
-    },
-    date: {
-      reg: /^\d{4}[./-]\d{2}[./-]\d{2}$/, //first type gggg-mm-dd and sec type dd-mm-gggg /^\d{2}[./-]\d{2}[./-]\d{4}$/
-    },
-    tel: {
-      // eslint-disable-next-line no-useless-escape
-      reg: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/, //russian tel and american tel /^\+?(\d{1,3})?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/,
-      minLength: 1,
-      maxLength: 20
-    },
-    file: {
-      size:  10025711, //10mb
-      type: "image",
-    },
-  };
-
-  constructor (el, options) {
+  constructor(el, options) {
     this.el = document.querySelector(el);
     this.options = options;
     this.render();
     this.setup();
-  };
+  }
 
-  get validObj (){
-    return this.validObj;
-  };
-
-  set validObj (obj){
-    this.validObj = obj;
-  };
-
-  getTemplate (inputs = [], textarea = [], btn = [], error = false, formClass = "validator-form", blockClass="block", errorClass = "block__error", labelClass = "block__label") { // method what return inputs, button, and full form
-    const input = inputs.map(i => { // template of input
-      if(error) {
-        return `<div class="${blockClass}">
-                <div class="${errorClass} error-${i.id}">${i.error}</div>
-                <input class="validator ${i.class} ${i.id}" data-type="${i.id}" data-valid="true" type="${i.type}" placeholder="${i.placeholder}" id="${i.id}"/>
-                <label class="${labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
-              </div>`
-      } else {
-        return `<div class="${blockClass}">
-                <input class="validator ${i.class} ${i.id}" data-type="${i.id}" data-valid="true" type="${i.type}" placeholder="${i.placeholder}" id="${i.id}"/>
-                <label class="${labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
-              </div>`
+  // (inputs = [], textarea = [], btn = [], error = false, formClass = "validator-form", blockClass="block", errorClass = "block__error", labelClass = "block__label")
+  getTemplate() { // method what return inputs, button, and full form
+    const input = this.options.inputs.map((i) => { // template of input
+      if (this.options.error) {
+        return `<div class="${this.options.blockClass}">
+                  <div class="${this.options.errorClass} error-${i.id}">${i.error}</div>
+                  <input class="validator ${i.class} ${i.id}" data-type="${i.id}" data-valid="true" type="${i.type}" placeholder="${i.placeholder}" id="${i.id}"/>
+                  <label class="${this.options.labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
+                  </div>`;
       }
+      return `<div class="${this.options.blockClass}">
+                <input class="validator ${i.class} ${i.id}" data-type="${i.id}" data-valid="true" type="${i.type}" placeholder="${i.placeholder}" id="${i.id}"/>
+                <label class="${this.options.labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
+              </div>`;
     });
-
-  const text = textarea.map(i => { //template of textarea
-    if(error) {
-      return `<div class="${blockClass}">
-                <div class="${errorClass} error-${i.id}">${i.error}</div>
-                <textarea class="validator block__input ${i.class}" data-valid="true" placeholder="${i.placeholder}" id="${i.id}"></textarea>
-                <label class="${labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
-              </div>`
-    } else {
-      return `<div class="${blockClass}">
+    const text = this.options.textarea.map((i) => { // template of textarea
+      if (this.options.error) {
+        return `<div class="${this.options.blockClass}">
+                  <div class="${this.options.errorClass} error-${i.id}">${i.error}</div>
+                  <textarea class="validator block__input ${i.class}" data-valid="true" placeholder="${i.placeholder}" id="${i.id}"></textarea>
+                  <label class="${this.options.labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
+                </div>`;
+        }
+      return `<div class="${this.options.blockClass}">
                 <textarea class="validator block__input ${i.class}" data-valid="true" data-type="${i.id}" placeholder="${i.placeholder}" id="${i.id}"></textarea>
-                <label class="${labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
-              </div>`
-    }
-  });
-
-    const button = btn.map(i => { // template button
-      return `<button class="validator-btn ${i.class}" type="${i.type}">${i.text}</button>`
+                <label class="${this.options.labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
+              </div>`;
     });
-    return `<form class="${formClass}" method="POST">
+    const btn = this.options.btn.map((i) => `<button class="validator-btn ${i.class}" type="${i.type}">${i.text}</button>`); // template of button
+    return `<form class="${this.options.formClass}" method="POST">
               ${input.join('')}
               ${text.join('')}
-              ${button.join('')}
+              ${btn.join('')}
             </form>`;
-  };
+  }
+  //   const input = inputs.map(i => { // template of input
+  //     if(error) {
+  //       return `<div class="${blockClass}">
+  //               <div class="${errorClass} error-${i.id}">${i.error}</div>
+  //               <input class="validator ${i.class} ${i.id}" data-type="${i.id}" data-valid="true" type="${i.type}" placeholder="${i.placeholder}" id="${i.id}"/>
+  //               <label class="${labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
+  //             </div>`
+  //     } else {
+  //       return `<div class="${blockClass}">
+  //               <input class="validator ${i.class} ${i.id}" data-type="${i.id}" data-valid="true" type="${i.type}" placeholder="${i.placeholder}" id="${i.id}"/>
+  //               <label class="${labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
+  //             </div>`
+  //     }
+  //   });
 
-  useForm(){
+  // const text = textarea.map(i => { //template of textarea
+  //   if(error) {
+  //     return `<div class="${blockClass}">
+  //               <div class="${errorClass} error-${i.id}">${i.error}</div>
+  //               <textarea class="validator block__input ${i.class}" data-valid="true" placeholder="${i.placeholder}" id="${i.id}"></textarea>
+  //               <label class="${labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
+  //             </div>`
+  //   } else {
+  //     return `<div class="${blockClass}">
+  //               <textarea class="validator block__input ${i.class}" data-valid="true" data-type="${i.id}" placeholder="${i.placeholder}" id="${i.id}"></textarea>
+  //               <label class="${labelClass} label-${i.id}" for="${i.id}">${i.label}</label>
+  //             </div>`
+  //   }
+  // });
+  //   const button = btn.map(i => { // template button
+  //     return `<button class="validator-btn ${i.class}" type="${i.type}">${i.text}</button>`
+  //   });
+  //   return `<form class="${formClass}" method="POST">
+  //             ${input.join('')}
+  //             ${text.join('')}
+  //             ${button.join('')}
+  //           </form>`;
+  // };
+
+  useForm() {
     return true
-  };
+  }
 
-  render () { // require full form
-    const {inputs, textarea, btn, errorMessages, formClass, blockClass, errorClass, labelClass} = this.options;
-    const {custom} = this.options;
-    const {renderForm} = this.options;
+  render() { // require full form
+    const { custom } = this.options;
+    const { renderForm } = this.options;
     this.customObject(custom);
-    if(renderForm){ // if user use render form
-      this.el.innerHTML = this.getTemplate(inputs, textarea, btn, errorMessages, formClass, blockClass, errorClass, labelClass);
-    } else { //if user use his form
+    if (renderForm) { // if user use render form
+      this.el.innerHTML = this.getTemplate(this.options);
+    } else { // if user use his form
       this.useForm();
     }
-  };
+  }
 
-  setup () { // req clickhandler
+  setup() { // req clickhandler
     this.focusBlurHandler = this.focusBlurHandler.bind(this);
     const inputsList = document.querySelectorAll(".validator");
-    inputsList.forEach(i => {
-      i.addEventListener('focus', this.focusBlurHandler),
-      i.addEventListener('blur', this.focusBlurHandler)
+    inputsList.forEach((i) => {
+      i.addEventListener('focus', this.focusBlurHandler);
+      i.addEventListener('blur', this.focusBlurHandler);
     });
     const btn = document.querySelector(".validator-btn");
     this.clickHandler = this.clickHandler.bind(this);
-    btn.addEventListener('click', this.clickHandler);
-  };
+    // btn.addEventListener('click', this.clickHandler);
+  }
 
-  focusBlurHandler (event) { //handler events
+  focusBlurHandler(event) { // handler events
     const input = event.target;
     switch (event.type) {
       case "focus":
-        if (input.id == "date") input.type = "date"
+        if (input.id == "date") input.type = "date";
         input.classList.add("js-input-focus");
         break;
       case "blur":
@@ -136,29 +160,29 @@ class CheckForm {
       default:
         break;
     }
-  };
+  }
 
-  cycleObj (name, key, value) {
-    for (const z in this.validObj) { //cycle on validobj
+  cycleObj(name, key, value) {
+    for (const z in this.validObj) { // cycle on validobj
       if (this.validObj.hasOwnProperty(z)) { // z - name of field, this.validObj[z] - field
         for (const y in this.validObj[z]) {
           if (this.validObj[z].hasOwnProperty(y)) {
-            if (z === name)
-              if(y === key)
-                this.validObj[z][y] = value;
+            if (z === name) {
+if (y === key) { this.validObj[z][y] = value; }
+}
           }
         }
       }
     }
-  };
+  }
 
-  customObject (custom) { //customization validObj
-    for (let i = 0; i < custom.length; i++) { //cycle on custom mas
-      for (const j in custom[i]) { //cycle on custom mas obj's
-        if (custom[i].hasOwnProperty(j)) { //j - name of type input, custom[i] - object witn custom fields
-          for (const k in custom[i][j]) { //cycle on obj's fields
-            if (custom[i][j].hasOwnProperty(k)) { //k - name of custom key, custom[i][j] - custom object, custom[i][j][k] - custom value
-              this.cycleObj(j, k, custom[i][j][k])
+  customObject(custom) { // customization validObj
+    for (let i = 0; i < custom.length; i++) { // cycle on custom mas
+      for (const j in custom[i]) { // cycle on custom mas obj's
+        if (custom[i].hasOwnProperty(j)) { // j - name of type input, custom[i] - object witn custom fields
+          for (const k in custom[i][j]) { // cycle on obj's fields
+            if (custom[i][j].hasOwnProperty(k)) { // k - name of custom key, custom[i][j] - custom object, custom[i][j][k] - custom value
+              this.cycleObj(j, k, custom[i][j][k]);
             }
           }
         }
@@ -166,11 +190,11 @@ class CheckForm {
     }
   };
 
-  showErrorMessage (block) {
+  showErrorMessage(block) {
     let parent = block.parentElement;
-    if(this.options.errorClass){
-      if(parent.querySelector('.' + this.options.errorClass)){
-        let errorBlock = parent.querySelector('.' + this.options.errorClass);
+    if (this.options.errorClass) {
+      if (parent.querySelector(`.${this.options.errorClass}`)) {
+        let errorBlock = parent.querySelector(`.${this.options.errorClass}`);
         errorBlock.style.opacity = '1';
         block.classList.add("error");
         block.classList.remove("good");
@@ -178,8 +202,7 @@ class CheckForm {
         block.classList.add("error");
         block.classList.remove("good");
       }
-    } else {
-      if(parent.querySelector('.block__error')){
+    } else if (parent.querySelector('.block__error')) {
         let errorBlock = parent.querySelector('.block__error');
         errorBlock.style.opacity = '1';
         block.classList.add("error");
@@ -188,14 +211,13 @@ class CheckForm {
         block.classList.add("error");
         block.classList.remove("good");
       }
-    }
-  };
+  }
 
-  hideErrorMessage (block) {
+  hideErrorMessage(block) {
     let parent = block.parentElement;
-    if(this.options.errorClass){
-      if(parent.querySelector('.' + this.options.errorClass)){
-        let errorBlock = parent.querySelector('.' + this.options.errorClass);
+    if (this.options.errorClass) {
+      if (parent.querySelector(`.${this.options.errorClass}`)) {
+        let errorBlock = parent.querySelector(`.${this.options.errorClass}`);
         errorBlock.style.opacity = '0';
         block.classList.remove("error");
         block.classList.add("good");
@@ -203,8 +225,7 @@ class CheckForm {
         block.classList.remove("error");
         block.classList.add("good");
       }
-    } else {
-      if(parent.querySelector('.block__error')){
+    } else if (parent.querySelector('.block__error')) {
         let errorBlock = parent.querySelector('.block__error');
         errorBlock.style.opacity = '0';
         block.classList.remove("error");
@@ -213,74 +234,71 @@ class CheckForm {
         block.classList.remove("error");
         block.classList.add("good");
       }
-    }
-  };
+  }
 
-  regCheck (val, block, reg, minLength = 0, maxLength = 1000) { // function for check regexp
+  regCheck(val, block, reg, minLength = 0, maxLength = 1000) { // function for check regexp
     if (val.length < minLength || val.length > maxLength) {
       this.showErrorMessage(block);
-    } else {
-      if (!reg.test(val)) {
+    } else if (!reg.test(val)) {
         this.showErrorMessage(block);
       } else {
         this.hideErrorMessage(block);
         this.checkSubstring(block, val);
         this.checkBlackList(block, val);
       }
-    }
-  };
+  }
 
-  checkSubstring (block, val) { //check string for include substr
+  checkSubstring(block, val) { // check string for include substr
     for (let i = 0; i < this.options.checkSubstr.length; i++) {
-      if(this.options.checkSubstr[i].id === block.getAttribute("id")) {
-        if(val.includes(this.options.checkSubstr[i].substr)){
+      if (this.options.checkSubstr[i].id === block.getAttribute("id")) {
+        if (val.includes(this.options.checkSubstr[i].substr)) {
           this.hideErrorMessage(block);
         } else {
           this.showErrorMessage(block);
         }
       } else {
-        return true
+        return true;
       }
     }
   }
 
-  checkBlackList (block, val) { //check string for include substr from blacklist
+  checkBlackList(block, val) { // check string for include substr from blacklist
     for (let i = 0; i < this.options.blackList.length; i++) {
-      if(val.includes(this.options.blackList[i])) {
+      if (val.includes(this.options.blackList[i])) {
         this.showErrorMessage(block);
       }
     }
   }
 
-  clickHandler (event) { // validation when click submit
+  clickHandler(event) { // validation when click submit
     event.preventDefault();
     const inputsList = document.querySelectorAll("[data-valid=true]");
-    inputsList.forEach(block => {
+    inputsList.forEach((block) => {
       let val = block.value;
       switch (block.getAttribute("data-type")) { // all inputs should have id
         case "nickname":
-          let nicknameReg = new RegExp(this.validObj.nickname.reg);
-          this.regCheck(val, block, nicknameReg, this.validObj.nickname.minLength, this.validObj.nickname.maxLength);
+          let nicknameReg = new RegExp(validObj.nickname.reg);
+          this.regCheck(val, block, nicknameReg, validObj.nickname.minLength, validObj.nickname.maxLength);
           break;
         case "fullname":
-          let nameReg = new RegExp(this.validObj.fullName.reg);
-          this.regCheck(val, block, nameReg, this.validObj.fullName.minLength, this.validObj.fullName.maxLength);
+          let nameReg = new RegExp(validObj.fullName.reg);
+          this.regCheck(val, block, nameReg, validObj.fullName.minLength, validObj.fullName.maxLength);
           break;
         case "email":
-          let emailReg = new RegExp(this.validObj.email.reg);
-          this.regCheck(val, block, emailReg, this.validObj.email.minLength, this.validObj.email.maxLength);
+          let emailReg = new RegExp(validObj.email.reg);
+          this.regCheck(val, block, emailReg, validObj.email.minLength, validObj.email.maxLength);
           break;
         case "password":
-          let passwordReg = new RegExp(this.validObj.password.reg);
+          let passwordReg = new RegExp(validObj.password.reg);
           this.regCheck(val, block, passwordReg);
           break;
         case "date":
-          let dateReg = new RegExp(this.validObj.date.reg);
+          let dateReg = new RegExp(validObj.date.reg);
           this.regCheck(val, block, dateReg);
           break;
         case "tel":
-          let telReg = new RegExp(this.validObj.tel.reg);
-          this.regCheck(val, block, telReg, this.validObj.tel.minLength, this.validObj.tel.maxLength);
+          let telReg = new RegExp(validObj.tel.reg);
+          this.regCheck(val, block, telReg, validObj.tel.minLength, validObj.tel.maxLength);
           break;
         case "checkbox":
           if (!block.checked) {
@@ -311,18 +329,18 @@ class CheckForm {
           }
           break;
         default:
-          console.log("def");
+          break;
       }
     });
-  };
+  }
 
-  validOk(funcName){
-    document.querySelector('.validator-btn').addEventListener('click',function () {
+  validOk(funcName) {
+    document.querySelector('.validator-btn').addEventListener('click', function () {
       const inputsList = document.querySelectorAll("[data-valid=true]");
       const goodInputs = document.querySelectorAll(".good");
-      if (inputsList.length === goodInputs.length){
+      if (inputsList.length === goodInputs.length) {
         funcName();
-      } else return false
-    })
+      } else return false;
+    });
   }
-};
+}
